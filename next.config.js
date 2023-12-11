ee8976b195c67314ba3280e3801e6274df1f9b46
E-paiva <141731995+E-paiva@users.noreply.github.com>
2023-12-11 21:50:37 +0000

    deploy config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+   output:'export',
+   distDir:'dist'
+
 
  // Add basePath 
  //basePath: '/github-pages',
